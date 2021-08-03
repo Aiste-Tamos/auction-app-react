2627faf0ce8f4c864fc5c5b7da5ea58af66f3ee0
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-08-03 09:56:42 +0300

    added some style to auctions list items

--- src/components/auctionsList/Auction.jsx
+++ src/components/auctionsList/Auction.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import './auction.scss';
  
 export const Auction = ({ auction, auctionState, onBtnClick }) => {
+    const mainClassName = "auction";
+    const titleClass = `${mainClassName}__title`;
+    const descriptionClass = `${mainClassName}__description`;
     
     return (
         <div id={auction.id} name="auction" value={auction.id} className="auction">
-            {auction.name}
-            <span>{auction.description}</span>
+            <h3 className={titleClass}>{auction.name}</h3>
+            <span className={descriptionClass}>{auction.description}</span>
             <button onClick={onBtnClick}>{auctionState}</button>
         </div> 
    );
--- src/components/auctionsList/AuctionsList.jsx
+++ src/components/auctionsList/AuctionsList.jsx
@@ -5,8 +5,10 @@ import "./auctionsList.scss";
 import { Auction } from "./Auction";
 
 export const AuctionsList = ({ auctions, auctionState, onBtnClick }) => {
+    const mainClassName = "auction-list";
+    
     return (
-        <div className="to-do-list">
+        <div className={mainClassName}>
             {auctions.map((auction, id) => {
                 return (
                     <Auction auction={auction} key={id} auctionState={auctionState} onBtnClick={onBtnClick}/>
