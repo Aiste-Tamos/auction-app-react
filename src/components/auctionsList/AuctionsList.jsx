af6ab007d88d071450ea9153545b75ca2095d2cf
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-08-05 13:26:23 +0300

    small fix for changeAuctionState function

--- src/components/auctionsList/AuctionsList.jsx
+++ src/components/auctionsList/AuctionsList.jsx
@@ -1,28 +1,17 @@
-import React, { useState} from "react";
+import React from "react";
 
 import "./auctionsList.scss";
 
 import { Auction } from "./Auction";
 
-export const AuctionsList = ({ auctions }) => {
-    const [auctionState, setAuctionState] = useState("start");
+export const AuctionsList = ({ auctions, auctionState, onBtnClick }) => {
     const mainClassName = "auction-list";
-
-    const changeAuctionState = () => {
-            var timeleft = 60;
-            var downloadTimer = setInterval(() => {
-            timeleft--;
-            setAuctionState(`00:${(timeleft).toLocaleString('en-US', {minimumIntegerDigits: 2})}`);
-            if(timeleft <= 0)
-                clearInterval(downloadTimer);
-            }, 1000);
-        }
 
     return (
         <div className={mainClassName}>
             {auctions.map((auction, id) => {
                 return (
-                    <Auction auction={auction} key={id} auctionState={auctionState} onBtnClick={changeAuctionState}/>
+                    <Auction auction={auction} key={id} auctionState={auctionState} onBtnClick={onBtnClick}/>
                 )
             })}
         </div>
--- src/components/mainContent/MainContent.jsx
+++ src/components/mainContent/MainContent.jsx
@@ -7,13 +7,14 @@ import "./mainContent.scss";
 export const MainContent = () => {
 
   const [auctions, setAuctions] = useState([]);
+  const [auctionState, setAuctionState] = useState("start");
+  const [clicked, setClicked] = useState(false);
 
   const mainClassName = "main-content";
-  
+
   useEffect(() => {
     const auctionsList = JSON.parse(localStorage.getItem('auctions'));
     if (auctionsList) {
-      console.log(auctionsList);
       setAuctions(auctionsList);
     }
   }, []);
@@ -29,10 +30,23 @@ export const MainContent = () => {
     setAuctions(copy);
  }
 
+ const changeAuctionState = () => {
+   if (!clicked) {
+    var timeleft = 60;
+    var downloadTimer = setInterval(() => {
+    timeleft--;
+    setAuctionState(`00:${(timeleft).toLocaleString('en-US', {minimumIntegerDigits: 2})}`);
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    }, 1000);
+    setClicked(true);
+   }
+}
+
   return (
     <div className={mainClassName}>
         <AuctionForm minNameLength="4" minDescriptionLength="4" addAuction={addAuction}/>
-        <AuctionsList auctions={auctions}/>
+        <AuctionsList auctions={auctions} auctionState={auctionState} onBtnClick={changeAuctionState}/>
     </div>
   );
 };
